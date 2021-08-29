6ff5a40b29a1d67f8826e3caf8f14d55e37b0d98
RayRappa <87951684+RayRappa@users.noreply.github.com>
2021-08-28 20:19:19 -0700

    python challenge

---
+++ PyPoll/main.py
@@ -0,0 +1,43 @@
+import os
+
+import csv
+
+#election_csv = os.path.join("..", "Resources", "election_data.csv")
+election_csv = "Resources\election_data.csv"
+
+list = ["Voter_ID", "county", "candidate"]
+
+print("\nList Items: ")
+print(list[0])
+print(list[1])
+print(list[2])
+
+
+#Sum of column 1 votes for total votes
+
+
+
+
+
+total_votes = total_votes + 1
+#Acomplete list of candidates who received votes (candidates)
+# loop thru names and append name in a dictionary if not in list already
+
+
+
+
+#The percentage of votes each candidate won
+
+
+
+
+#The total number of votes each candidate won
+
+
+#The winner of the election based on popular vote
+# create a candidate list and get teh max as rthe winner
+
+
+
+
+#
